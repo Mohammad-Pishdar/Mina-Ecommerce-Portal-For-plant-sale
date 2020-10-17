a2f44e394f31d976336313915f0aec3d04d1836f
Mo Pishdar <mohpish@gmail.com>
2020-10-18 00:38:10 +1100

    first route created in server that shows all the items in our database as a json file

--- mina-backend/server.js
+++ mina-backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import data from './data.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+app.get('/api/items', (req, res) => {
+    res.send(data.items);
+});
+
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
 });
