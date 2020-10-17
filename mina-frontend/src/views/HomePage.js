5e455660410f8b7e68d83c96aff3dd654ef46660
Mo Pishdar <mohpish@gmail.com>
2020-10-18 04:35:13 +1100

    added loading and error screens for when the app tries to load data from backend or failed to fetch data from backend

--- mina-frontend/src/views/HomePage.js
+++ mina-frontend/src/views/HomePage.js
@@ -1,72 +1,87 @@
 import React, { useEffect, useState } from 'react';
 import Item from '../components/Item';
 import axios from 'axios';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomePage() {
 
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const getData = async () => {
-      const {data} = await axios.get('/api/items');
-      setItems(data);
+      try {
+        setLoading(true);
+        const {data} = await axios.get('/api/items');
+        setLoading(false);
+        setItems(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      } 
     };
     getData();
   }, [])
 
     return (
-        <div className="container">
+        <div className="container mainPageContainer">
+          {loading? <LoadingBox></LoadingBox> : error? <MessageBox variant="danger">{error}</MessageBox> : 
+          <div className="row">
 
-    <div className="row">
+<div className="col-lg-3">
 
-      <div className="col-lg-3">
+  <h1 className="my-4">Shop Name</h1>
+  <div className="list-group">
+    <a href="#" className="list-group-item">Category 1</a>
+    <a href="#" className="list-group-item">Category 2</a>
+    <a href="#" className="list-group-item">Category 3</a>
+  </div>
 
-        <h1 className="my-4">Shop Name</h1>
-        <div className="list-group">
-          <a href="#" className="list-group-item">Category 1</a>
-          <a href="#" className="list-group-item">Category 2</a>
-          <a href="#" className="list-group-item">Category 3</a>
-        </div>
+</div>
 
+<div className="col-lg-9">
+
+  <div id="carouselExampleIndicators" className="carousel slide my-4" data-ride="carousel">
+    <ol className="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div className="carousel-inner" role="listbox">
+      <div className="carousel-item active">
+        <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide"/>
       </div>
-
-      <div className="col-lg-9">
-
-        <div id="carouselExampleIndicators" className="carousel slide my-4" data-ride="carousel">
-          <ol className="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div className="carousel-inner" role="listbox">
-            <div className="carousel-item active">
-              <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide"/>
-            </div>
-            <div className="carousel-item">
-              <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide"/>
-            </div>
-            <div className="carousel-item">
-              <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide"/>
-            </div>
-          </div>
-          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
-        <div className="row">
-          {
-            items.map(item =>(
-              <Item key={item._id} item={item}></Item>
-            ))
-          }
-          </div>
+      <div className="carousel-item">
+        <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide"/>
       </div>
-
+      <div className="carousel-item">
+        <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide"/>
+      </div>
     </div>
+    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="sr-only">Previous</span>
+    </a>
+    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="sr-only">Next</span>
+    </a>
+  </div>
+  <div className="row">
+    {
+      items.map(item =>(
+        <Item key={item._id} item={item}></Item>
+      ))
+    }
+    </div>
+</div>
+
+</div>
+          }
+
+    
 
   </div>
     )
