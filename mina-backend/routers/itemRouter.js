b43199ae4c0fee04b7a8fb5a6e56445dc7f20f31
Mo Pishdar <mohpish@gmail.com>
2020-10-21 20:49:38 +1100

    added a getter function inside the item router to get the list of all the items and send it back to the frontend upon request

--- mina-backend/routers/itemRouter.js
+++ mina-backend/routers/itemRouter.js
@@ -6,10 +6,19 @@ import Item from '../models/item.js';
 const itemRouter = express.Router();
 
 itemRouter.get('/seed', expressAsyncHandler(async (req, res) => {
+    //this ensures all the items will be removed before seeding the database with new items
+    // await Item.remove({});
     const createdItems = await Item.insertMany(data.items);
     res.send({
         createdItems
     });
+}));
+
+//creating an API to send the list of items to frontend. Note that the / we use here for the URL parameter of the get method will be added to the end of /api/items since we define it here in item router.
+itemRouter.get('/', expressAsyncHandler(async (req, res) => {
+    //getting the list of items. Using an empty object ensures that it finds all the items.
+    const items = await Item.find({});
+    res.send(items);
 }));
 
 export default itemRouter;
