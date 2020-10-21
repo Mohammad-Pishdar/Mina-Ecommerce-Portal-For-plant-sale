175ca253c756d39738d9e810c25156b85e67abf3
Mo Pishdar <mohpish@gmail.com>
2020-10-21 20:32:08 +1100

    server.js updated with the new routers. Items seed route works and data displayed

--- mina-backend/server.js
+++ mina-backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
-import data from "./data.js";
+//data.js is no longer needed
+// import data from "./data.js";
 import mongoose from "mongoose";
 import userRouter from "./routers/userRouter.js";
+import itemRouter from "./routers/itemRouter.js";
 
 const app = express();
 //connecting to mongoDB databse. By adding the name of your databse at the end of the URI of mongoDB you create a databse. Here we also make the URI dynamci instead a hard coded static one just like we did with our port down below 
@@ -16,20 +18,22 @@ app.get("/", (req, res) => {
     res.send("Server is ready");
 });
 
-app.get("/api/items", (req, res) => {
-    res.send(data.items);
-});
+//Now that we connected our database to our server.js file below, there is no need to use static data.js file to get the items data so we can cooment this out
+// app.get("/api/items", (req, res) => {
+//     res.send(data.items);
+// });
 
-app.get("/api/items/:id", (req, res) => {
-    const item = data.items.find((item) => item._id === req.params.id);
-    if (item) {
-        res.send(item);
-    } else {
-        res.status(404).send({
-            message: "Item Not Found"
-        });
-    }
-});
+//The same goes for this route. We don't this anymore either since we want to implement these routes using the routers we created so we comment this out too
+// app.get("/api/items/:id", (req, res) => {
+//     const item = data.items.find((item) => item._id === req.params.id);
+//     if (item) {
+//         res.send(item);
+//     } else {
+//         res.status(404).send({
+//             message: "Item Not Found"
+//         });
+//     }
+// });
 
 //we use our userRouter here 
 app.use('/api/users', userRouter);
@@ -40,6 +44,8 @@ app.use((err, req, res, next) => {
         message: err.message
     });
 })
+
+app.use('/api/items', itemRouter);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
