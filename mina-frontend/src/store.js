8febaddd6b44a74c7a75088837594913169a34df
Mo Pishdar <mohpish@gmail.com>
2020-10-22 19:37:47 +1100

    user's name and other info saved and retrieved from local stroage successfully so it's there even when you refresh the page

--- mina-frontend/src/store.js
+++ mina-frontend/src/store.js
@@ -9,13 +9,21 @@ import {
     itemDetailsReducer,
     itemListReducer
 } from './reducers/itemsReducers';
-import { shoppingCartReducer } from './reducers/shoppingCartReducers';
-import { userSignInReducer } from './reducers/userReducers';
+import {
+    shoppingCartReducer
+} from './reducers/shoppingCartReducers';
+import {
+    userSignInReducer
+} from './reducers/userReducers';
 
 const initialState = {
     //defining a default value for our shopping cart and read content of the local storage to populate our shopping cart if there is something in local storage upon each refresh of the page or make the shopping cart empty if there is not
-    shoppingCart:{
-        shoppingCartItems: localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems')) : []
+    shoppingCart: {
+        shoppingCartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+    },
+    //Doing exactly the same thing for user sign in info to retrieve data from local storage if there is any
+    signIn: {
+        userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
     }
 };
 const reducer = combineReducers({
