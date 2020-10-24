0f4b3de694e6d2dbd8493a6a4a23760d7ac7090e
Mo Pishdar <mohpish@gmail.com>
2020-10-24 16:27:37 +1100

    save payment method action was added as a switch statement case in shopping cart reducers file

--- mina-frontend/src/reducers/shoppingCartReducers.js
+++ mina-frontend/src/reducers/shoppingCartReducers.js
@@ -1,5 +1,8 @@
 import {
-    ADD_TO_CART, REMOVE_FROM_CART, SAVE_SHIPPING_ADDRESS
+    ADD_TO_CART,
+    REMOVE_FROM_CART,
+    SAVE_PAYMENT_METHOD,
+    SAVE_SHIPPING_ADDRESS
 } from "../constants/shoppingCartConstants";
 
 //we set the default value of state here as on object that contains an empty array needs to be filled with items in shopping cart
@@ -29,22 +32,26 @@ export const shoppingCartReducer = (state = {
                     shoppingCartItems: [...state.shoppingCartItems, item]
                 };
             }
-        //creating another case this time for the remove from cart action
-        case REMOVE_FROM_CART:
-            return {
-                ...state,
-                // here we filter out the shooping cart for the item whose id equals to the id returned by the action payload which means that item will be removed from the cart while we keep all the other items. This way the reducer will update the redux store
-                shoppingCartItems: state.shoppingCartItems.filter(x => x.item !== action.payload)
-            };
-        //adding save shipping address case
-        case SAVE_SHIPPING_ADDRESS:
-            //update the previous state shipping address to whatever the user submits
-            return {
-                ...state,
-                shippingAddress: action.payload
-            };
-
-        default:
-            return state;
+            //creating another case this time for the remove from cart action
+            case REMOVE_FROM_CART:
+                return {
+                    ...state,
+                    // here we filter out the shooping cart for the item whose id equals to the id returned by the action payload which means that item will be removed from the cart while we keep all the other items. This way the reducer will update the redux store
+                    shoppingCartItems: state.shoppingCartItems.filter(x => x.item !== action.payload)
+                };
+                //adding save shipping address case
+            case SAVE_SHIPPING_ADDRESS:
+                //update the previous state shipping address to whatever the user submits
+                return {
+                    ...state,
+                    shippingAddress: action.payload
+                };
+                //adding save payment method case
+            case SAVE_PAYMENT_METHOD:
+                return {
+                    ...state, paymentMethod: action.payload
+                };
+            default:
+                return state;
     }
 }
