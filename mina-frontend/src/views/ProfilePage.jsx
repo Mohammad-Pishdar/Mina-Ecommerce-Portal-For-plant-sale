8d2c13e8e1ee081ea1f2a11787e8d92c44ec509f
Mo Pishdar <mohpish@gmail.com>
2020-10-26 21:56:30 +1100

    profile page updated by putting the fields that needed to change on state

--- mina-frontend/src/views/ProfilePage.jsx
+++ mina-frontend/src/views/ProfilePage.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userDetails } from "../actions/userActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 
 export default function ProfilePage() {
+  //since we need to update databse with what user puts here in input fields we have to define state for them
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+
   //getting user information from redux store
   const userInformation = useSelector((state) => state.signIn);
   const { userInfo } = userInformation;
@@ -13,13 +19,25 @@ export default function ProfilePage() {
   const { loading, error, user } = detailsOfUser;
 
   const dispatch = useDispatch();
+
+  //here we add user as a dependency to useEffect so when user changes from null to an object from backend (when we send ajax request to backend from our userDetails action) useEffect runs again. So if the user is null it will run and fill the input fields with data from backend else if the user is already filled it will fill name and email with data from backend
   useEffect(() => {
-    dispatch(userDetails(userInfo._id));
-  }, [dispatch, userInfo._id]);
+    if (!user) {
+      dispatch(userDetails(userInfo._id));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, [dispatch, userInfo._id, user]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-  }
+    if (password !== confirmedPassword) {
+      alert("Your password and confirmed password should match");
+    } else {
+      dispatch(updateUserProfile({ userId: user._id, name, email, password }));
+    }
+  };
 
   return (
     <div className="row">
@@ -38,7 +56,8 @@ export default function ProfilePage() {
                 className="form-control"
                 id="name"
                 placeholder="Enter Name"
-                value={user.name}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className="form-group">
@@ -49,7 +68,8 @@ export default function ProfilePage() {
                 id="email"
                 aria-describedby="emailHelp"
                 placeholder="Enter Your Email"
-                value={user.email}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="form-group">
@@ -59,6 +79,7 @@ export default function ProfilePage() {
                 className="form-control"
                 id="password"
                 placeholder="Enter Your Password"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className="form-group">
@@ -68,6 +89,7 @@ export default function ProfilePage() {
                 className="form-control"
                 id="confirmPassword"
                 placeholder="Confirm Your Password"
+                onChange={(e) => setConfirmedPassword(e.target.value)}
               />
             </div>
             <button type="submit" className="btn btn-primary btn-block">
