cea37a393e2fdb27ded8a9631e32f480f75c73e0
Mo Pishdar <mohpish@gmail.com>
2020-10-26 16:52:36 +1100

    order list reducer added to order reducers file

--- mina-frontend/src/reducers/orderReducers.js
+++ mina-frontend/src/reducers/orderReducers.js
@@ -6,6 +6,9 @@ import {
     ORDER_DETAILS_FAILURE,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
+    ORDER_LIST_FAILURE,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
     ORDER_PAY_FAILURE,
     ORDER_PAY_REQUEST,
     ORDER_PAY_RESET,
@@ -81,6 +84,28 @@ export const orderPayReducer = (state = {}, action) => {
             };
         case ORDER_PAY_RESET:
             return {};
+        default:
+            return state;
+    }
+}
+
+//For this reducer we set the default value to an empty array that needs to be filled with orders
+export const orderListReducer = (state = {
+    orders: []
+}, action) => {
+    switch (action.type) {
+        case ORDER_LIST_REQUEST:
+            return {
+                loading: true
+            };
+        case ORDER_LIST_SUCCESS:
+            return {
+                loading: false, orders: action.payload
+            };
+        case ORDER_LIST_FAILURE:
+            return {
+                loading: false, error: action.payload
+            };
         default:
             return state;
     }
